b44ad4d9fbcfac4bc210fa4bfd5148e5dd356536
derricklai33 <derricklai33@hotmail.com>
2020-12-15 15:45:17 +1100

    Completed Day 6 part 2

--- day6/day6.js
+++ day6/day6.js
@@ -12,9 +12,10 @@ const getData = (data) => {
   return finalResult
 }
 
-const partA = (arr) => {
-  // Initialize count
-  let count = 0;
+// Unique array function. This returns all of the unique characters
+// for each group
+const uniqArr = (arr) => {
+  let array = [];
   // Joins all array element into a giant string.
   const result = arr.map(item => {
     return item.join('');
@@ -23,14 +24,57 @@ const partA = (arr) => {
     // Spread operator and using set. Set creates new object that can 
     // contain only uniq characters from the combined string
     let uniq = [...new Set(item)];
-    count += uniq.length;
+    array.push(uniq);
+  })
+  return array
+}
+
+// Part A, counts the unique characters that each group contains 
+// total the length of each array element and that's the answer
+const partA = (arr) => {
+  let count = 0;
+  arr.forEach(item => {
+    count += item.length
   })
   return count;
 }
 
+// 
+const partB = (arr, uniqArr) => {
+  let ansArr = [];
+  // First loop, iterating through all the group and unique element array
+  // returned from the uniqArr function
+  for (let i = 0; i < arr.length; i++) {
+    const result = arr[i];
+    const array = uniqArr[i];
+    // This loop is used to loop through the unique element array for 
+    // each group
+    for(let j = 0; j < array.length; j++) {
+      let charCount = 0;
+      result.forEach(item => {
+        // The .includes increments the count to check if each group's 
+        // person has the same characters
+        if (item.includes(`${array[j]}`)) {
+          charCount++;
+        }
+      })
+      // If the incremented count === the number of person, means everyone
+      // in the group answered 'Yes' for that question
+      if (charCount === result.length) {
+        ansArr.push(array[j]);
+      }
+    }
+  }
+  return ansArr.length;
+}
+
+
 // Invoke function
 const endData = getData(readInput);
-const PartAResult = partA(endData);
+const uniqueArray = uniqArr(endData);
+const partAResult = partA(uniqueArray);
+const partBResult = partB(endData, uniqueArray);
 
 // Console.log answer
-console.log("Answer for part A is: " + PartAResult);
+console.log("Answer for part A is: " + partAResult);
+console.log("Answer for Part 2 is: " + partBResult);
