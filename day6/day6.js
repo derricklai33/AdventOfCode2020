09c763bbb21716c564718bc50d55d7256bb87392
derricklai33 <derricklai33@hotmail.com>
2020-12-15 14:40:31 +1100

    Completed Day6 part 1

--- day6/day6.js
+++ day6/day6.js
@@ -1,4 +1,36 @@
 const fs = require('fs');
 const readInput = fs.readFileSync('./day6data.txt', 'utf8')
 
-console.log(readInput)
+const getData = (data) => {
+  const split = data.split('\n\n');
+  const result = split.map(item => {
+  return item.replace(/\n/g, " ");
+})
+  const finalResult = result.map(item => {
+    return item.split(' ');
+  })
+  return finalResult
+}
+
+const partA = (arr) => {
+  // Initialize count
+  let count = 0;
+  // Joins all array element into a giant string.
+  const result = arr.map(item => {
+    return item.join('');
+  })
+  result.forEach(item => {
+    // Spread operator and using set. Set creates new object that can 
+    // contain only uniq characters from the combined string
+    let uniq = [...new Set(item)];
+    count += uniq.length;
+  })
+  return count;
+}
+
+// Invoke function
+const endData = getData(readInput);
+const PartAResult = partA(endData);
+
+// Console.log answer
+console.log("Answer for part A is: " + PartAResult);
